43ac8331fa4c862d658d70a0a15ca5b1bb0741c6
Albin76 <albin.svensson@gmail.com>
2020-07-29 09:10:26 +0200

    Update espnow-sensor-bme280_MQTT.ino
    Some updates to increase stability. Error handling.

--- espnow-sensor-bme280_MQTT.ino
+++ espnow-sensor-bme280_MQTT.ino
@@ -28,7 +28,10 @@ uint8_t remoteMac[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
 
 #define WIFI_CHANNEL 1
 #define SLEEP_SECS 100 // 1 minute. can be removed for TPL5111
-#define SEND_TIMEOUT 200  // 245 millis seconds timeout. This does not work. If set to 450 it takes that. Do not get response I think. 
+#define SLEEP_SECS_SHORT 60 // 1 minute. Used when failed wifi or failed espnow init
+#define SEND_TIMEOUT 300  // 245 millis seconds timeout. This does not work. If set to 450 it takes that. Do not get response I think. 
+#define CONNECTION_TIMEOUT 1000  // 
+//#define USING_BATTERY
 #define USING_DEEPSLEEP
 //#define USING_TPL5110
 //#define USING_TPL5110_BARE
@@ -44,7 +47,23 @@ struct __attribute__((packed)) SENSOR_DATA {
     float battLevelNow;
 } sensorData;
 
-
+/*
+// New struct
+struct __attribute__((packed)) SENSOR_DATA {
+    int   checkstart;
+    int   sendno;
+    int   sensor;
+    char  MQTT_sensor_topic[15];
+    int   millis;
+    float temp;
+    float humidity;
+    float pressure;
+    float battery;
+    float spare1;
+    float spare2;
+    int checkend;
+} sensorData;
+*/
 
 BME280 bme280;
 
@@ -76,16 +95,31 @@ void setup() {
     digitalWrite(TPL5110DONEPIN, LOW);
   #endif  
 
+/*
+// For new struct.
+  sensorData.checkstart = 1111;
+  sensorData.sendno;
+  sensorData.millis = 0;
+  sensorData.spare1 = 0.0;
+  sensorData.spare2 = 0.0;
+  sensorData.checkend = 1111;
+*/
+
   sensorData.sensor = MQTT_SENSOR_NO;
   sensorData.channelID = 275152;
   strcpy(sensorData.MQTT_sensor_topic, MQTT_SENSOR_TOPIC);
   
   Serial.begin(115200); Serial.println();
+
+  #ifdef USING_BATTERY
   Serial.printf("Before Battery reading: %i\n", millis());
   batteryreading();
   Serial.printf("Before BME280 read: %i\n", millis());
+  #endif
+
   // read sensor first before awake generates heat
   readBME280();
+  checkreadings();
   Serial.printf("After BME280 read: %i\n", millis());
   //WiFi.mode(WIFI_STA); // Station mode for esp-now sensor node // these are not in the minimal example. Takes long time. +60ms
   //WiFi.disconnect();
@@ -97,22 +131,28 @@ void setup() {
 
   if (esp_now_init() != 0) {         
     Serial.println("*** ESP_Now init failed");  // If failed then go to sleep.
-    gotoSleep();
+    gotoSleep(SLEEP_SECS);
   }
   Serial.printf("Before Send: %i\n", millis());
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_add_peer(remoteMac, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);
+  
+  
+  /*
   SENDSTATUS = esp_now_register_send_cb([](uint8_t* mac, uint8_t sendStatus) {    // esp_now_register_send_cb to register sending callback function. 
-
-    //It will return ESP_NOW_SEND_SUCCESS in sending callback function if the data is received successfully on MAC layer. Otherwise, it will return ESP_NOW_SEND_FAIL. 
-  Serial.printf("send_cb, send done, status = %i\n", sendStatus);
+  //It will return ESP_NOW_SEND_SUCCESS in sending callback function if the data is received successfully on MAC layer. Otherwise, it will return ESP_NOW_SEND_FAIL. 
+  Serial.printf("send_cb, send done, status = %i\n", SENDSTATUS);  // var sendstatus
   Serial.printf("After Send register: %i\n", millis());
   //Was a go to sleep here before, but last part of message was cut. Probably possible to optimise so sleep is called at correct time. 
   callbackCalled = true;
   });
-
   callbackCalled = false;  // Ändrat
+  */
+
+  // testing vatiant.
+  SENDSTATUS = esp_now_register_send_cb(OnDataSent);
   
+ 
   uint8_t bs[sizeof(sensorData)];
   Serial.printf("Size of sensorData: ");
   Serial.println(sizeof(sensorData));
@@ -128,12 +168,20 @@ void setup() {
 
 // sends and then goes into a loop and waits for callback=1 or timeout.
 void loop() {  // Ändrat
-  if (millis() > SEND_TIMEOUT) {  //sendStatus==1 || 
+  if (millis() > SEND_TIMEOUT ) {  //sendStatus==1 || 
     Serial.printf("In loop sendStatus = %i\n", SENDSTATUS);
     Serial.print("Going to sleep: "); 
     Serial.println(millis()); 
-    gotoSleep();
+    //Serial.println("Delay for 100ms");
+    //delay(100);
+    gotoSleep(SLEEP_SECS);
   }
+}
+
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Status: ");
+  Serial.println(sendStatus);
+  delay(100);
 }
 
 void readBME280() {
@@ -154,14 +202,30 @@ void readBME280() {
 
   if (bme280.beginI2C() == false) //Begin communication over I2C
   {
-    Serial.println("The sensor did not respond. Please check wiring.");
-    gotoSleep();
+    Serial.println("The sensor did not respond. Please check wiring. Going to sleep.");
+    gotoSleep(SLEEP_SECS);
   }
   //Serial.print("bme280 init="); Serial.println(bme280.begin(), HEX);
   sensorData.temp = bme280.readTempC();
   sensorData.humidity = bme280.readFloatHumidity();
   sensorData.pressure = bme280.readFloatPressure() / 100.0;
   Serial.printf("temp=%01f, humidity=%01f, pressure=%01f\n", sensorData.temp, sensorData.humidity, sensorData.pressure);
+}
+
+// Not complete yet!
+void checkreadings(){
+  int var = 0;
+  while (var < 3) {
+    if (isnan(sensorData.temp) || isnan(sensorData.humidity) || isnan(sensorData.pressure) || sensorData.temp>50 || sensorData.temp<-40 || sensorData.humidity>100 || sensorData.humidity<0 || sensorData.pressure>13000 || sensorData.pressure<700) {
+      //Serial.println("ERROR: Unrealistic sensor readings. Retrying try no:");
+      Serial.printf("ERROR: Unrealistic sensor readings. Retrying. Try no: %i\n", var);
+      var++;
+      readBME280();
+      } else {
+      return;
+      }
+    }
+  gotoSleep(SLEEP_SECS);
 }
 
 void batteryreading(){
@@ -193,15 +257,15 @@ void sort(int a[], int size) {
     }
 }
 
-void gotoSleep() {
+void gotoSleep(int sleepSecs) {
 
   #ifdef USING_DEEPSLEEP
-  int sleepSecs = SLEEP_SECS; 
+  //int sleepSecs = SLEEP_SECS; // added as parameter in function instead
   Serial.printf("Up for %i ms, going to sleep for %i secs...\n", millis(), sleepSecs); 
   Serial.println(); 
   Serial.println();  
-  //ESP.deepSleep(sleepSecs * 1000000, RF_NO_CAL);
-  ESP.deepSleepInstant(sleepSecs * 1000000, RF_NO_CAL); // Längre tid och drog mer ström med core beta!!!
+  ESP.deepSleep(sleepSecs * 1000000, RF_NO_CAL);
+  //ESP.deepSleepInstant(sleepSecs * 1000000, RF_NO_CAL); // Längre tid och drog mer ström med core beta!!!
   #endif
 
   #ifdef USING_TPL5110  
